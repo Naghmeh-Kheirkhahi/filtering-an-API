ec6c1067950038474b429738582687312bccd54e
naghmeh <melodycharitable4774@gmail.com>
2024-10-11 01:23:19 +0200

    did all things except filtering

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ fetch('https://fakestoreapi.com/products/')
     console.log(data);
 
     data.forEach(product => {
-        document.body.innerHTML = `
-        `
+
+        let products = document.querySelector('.products');
+
+        for (let index = 0; index < data.length; index++) {
+            products.innerHTML += `
+            <div class="col-3">
+                <img src="${product.image}" alt="">
+                <h4>${product.title}</h4>
+                <p>${product.category}</p>
+                <p>${product.price} euros</p>
+                <p class="description">${product.description}</p>
+            </div>
+        `  
+        }
+        
+
+
     });
 });
+
+
